c32574f29dde935fa05f5ba50d042c6ecb563618
LisanzaTabby <lisanzatabitha@gmail.com>
2024-10-21 11:57:20 +0300

    initial commit

--- AIUApp/urls.py
+++ AIUApp/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns  = [
     path('', views.index, name='home'),
+    path('room/', views.rooms, name='room'),
 ]
--- AIUApp/views.py
+++ AIUApp/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context={}
-    return render(request, 'homepage.html', context)
+    return render(request, 'home.html', context)
+def rooms(request):
+    context={}
+    return render(request, 'room.html', context)
 
