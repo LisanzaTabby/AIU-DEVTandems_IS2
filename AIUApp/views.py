659e2be610397b9cd73c34f08d3721e680d4d800
LisanzaTabby <lisanzatabitha@gmail.com>
2024-10-22 14:04:32 +0300

    Initial Commit

--- AIUApp/views.py
+++ AIUApp/views.py
@@ -1,19 +1,12 @@
 from django.shortcuts import render
-
+from .models import Room
 # Create your views here.
-room_list = [
-    {'id':1, 'name':'Lets Learn HTML'},
-    {'id':2, 'name':'Lets Learn Python'},
-    {'id':3, 'name':'Lets Learn Django'},
-]
-def index(request):
-    return render(request, 'AIUApp/homepage.html', {'rooms': room_list})
-def rooms(request, pk):
-    room = None
-    for i in room_list:
-        if i['id'] == int(pk):
-            room = i
 
+def index(request):
+    rooms = Room.objects.all()
+    return render(request, 'AIUApp/homepage.html', {'rooms': rooms})
+def rooms(request, pk):
+    room = Room.objects.get(id=pk).order_by('id')
     context={'room': room}
     return render(request, 'AIUApp/room.html', context)
 
