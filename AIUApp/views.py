ba13819d32f40b86d2d02721c1af29ca62f26737
LisanzaTabby <lisanzatabitha@gmail.com>
2024-10-22 12:42:34 +0300

    initial commit

--- AIUApp/views.py
+++ AIUApp/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+room_list = [
+    {'id':1, 'name':'Lets Learn HTML'},
+    {'id':2, 'name':'Lets Learn Python'},
+    {'id':3, 'name':'Lets Learn Django'},
+]
 def index(request):
-    context={}
-    return render(request, 'homepage.html', context)
-def rooms(request):
-    context={}
-    return render(request, 'room.html', context)
+    return render(request, 'AIUApp/homepage.html', {'rooms': room_list})
+def rooms(request, pk):
+    room = None
+    for i in room_list:
+        if i['id'] == int(pk):
+            room = i
+
+    context={'room': room}
+    return render(request, 'AIUApp/room.html', context)
 
