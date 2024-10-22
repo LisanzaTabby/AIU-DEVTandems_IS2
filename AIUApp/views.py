e704f73ccae90355af315182a378044ed12b3c9f
LisanzaTabby <lisanzatabitha@gmail.com>
2024-10-22 09:02:23 +0300

    initial commit

--- AIUApp/views.py
+++ AIUApp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context={}
-    return render(request, 'home.html', context)
+    return render(request, 'homepage.html', context)
 def rooms(request):
     context={}
     return render(request, 'room.html', context)
