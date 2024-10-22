292e5e376efe6e1820a554cdaad0379cf51e2d22
LisanzaTabby <lisanzatabitha@gmail.com>
2024-10-22 14:09:57 +0300

    Initial Commit

--- AIUApp/views.py
+++ AIUApp/views.py
@@ -3,10 +3,10 @@ from .models import Room
 # Create your views here.
 
 def index(request):
-    rooms = Room.objects.all()
+    rooms = Room.objects.all().order_by('id')
     return render(request, 'AIUApp/homepage.html', {'rooms': rooms})
 def rooms(request, pk):
-    room = Room.objects.get(id=pk).order_by('id')
+    room = Room.objects.get(id=pk)
     context={'room': room}
     return render(request, 'AIUApp/room.html', context)
 
