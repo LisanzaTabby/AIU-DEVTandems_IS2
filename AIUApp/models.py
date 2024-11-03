dd30f90b38393411f2f4b46cc588bb158bc01340
LisanzaTabby <lisanzatabitha@gmail.com>
2024-11-03 23:34:36 +0300

    initial commit

--- AIUApp/models.py
+++ AIUApp/models.py
@@ -21,7 +21,7 @@ class Room(models.Model):
         return self.name
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE) #link it to the parent model Room
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
