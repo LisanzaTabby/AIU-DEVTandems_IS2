b283e7ed52e1f58759b668b31078f3103449532d
LisanzaTabby <lisanzatabitha@gmail.com>
2024-11-04 00:58:45 +0300

    CRUD

--- AIUApp/urls.py
+++ AIUApp/urls.py
@@ -5,4 +5,6 @@ urlpatterns  = [
     path('', views.index, name='home'),
     path('room/<str:pk>/', views.rooms, name='room'),
     path('create-room/', views.create_room, name='create-room'),
+    path('update-room/<str:pk>/', views.updateRoom, name='updateroom'),
+    path('delete-room/<str:pk>/', views.deleteRoom, name='deleteroom'),
 ]
--- AIUApp/views.py
+++ AIUApp/views.py
@@ -4,7 +4,7 @@ from .forms import RoomForm
 # Create your views here.
 
 def index(request):
-    rooms = Room.objects.all().order_by('id')
+    rooms = Room.objects.all()
     return render(request, 'AIUApp/homepage.html', {'rooms': rooms})
 def rooms(request, pk):
     room = Room.objects.get(id=pk)
@@ -20,4 +20,22 @@ def create_room(request):
             return redirect('home')
     context={'form':form}
     return render(request, 'AIUApp/room_form.html', context)
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)# plus telling it which room to update using the instance variable
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context={'form':form}
+    return render(request, 'AIUApp/room_form.html', context)
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'AIUApp/delete.html',{'obj':room})
+
+
 
