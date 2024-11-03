aaa84f39b34091e3b1ae037f5c7c50885a9f64ec
LisanzaTabby <lisanzatabitha@gmail.com>
2024-11-04 00:21:09 +0300

    CRUD

--- AIUApp/views.py
+++ AIUApp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 # Create your views here.
 
 def index(request):
@@ -9,4 +10,14 @@ def rooms(request, pk):
     room = Room.objects.get(id=pk)
     context={'room': room}
     return render(request, 'AIUApp/room.html', context)
+
+def create_room(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)# passing all tghe data from the form into the cariable form
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context={'form':form}
+    return render(request, 'AIUApp/room_form.html', context)
 
