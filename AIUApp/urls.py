17840bcd75dedc9307f2156fdb26f4d623fd4b5f
LisanzaTabby <lisanzatabitha@gmail.com>
2024-11-08 23:59:28 +0300

    restriction of pages

--- AIUApp/urls.py
+++ AIUApp/urls.py
@@ -4,8 +4,11 @@ from .import views
 urlpatterns  = [
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerPage, name='register'),
+
     path('', views.index, name='home'),
     path('room/<str:pk>/', views.rooms, name='room'),
+
     path('create-room/', views.create_room, name='create-room'),
     path('update-room/<str:pk>/', views.updateRoom, name='updateroom'),
     path('delete-room/<str:pk>/', views.deleteRoom, name='deleteroom'),
