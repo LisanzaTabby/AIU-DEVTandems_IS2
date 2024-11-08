c86981baa187ae9aa386e898b538e97e4638b0e5
LisanzaTabby <lisanzatabitha@gmail.com>
2024-11-08 23:39:37 +0300

    user logout feature

--- AIUApp/urls.py
+++ AIUApp/urls.py
@@ -3,6 +3,7 @@ from .import views
 
 urlpatterns  = [
     path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
     path('', views.index, name='home'),
     path('room/<str:pk>/', views.rooms, name='room'),
     path('create-room/', views.create_room, name='create-room'),
