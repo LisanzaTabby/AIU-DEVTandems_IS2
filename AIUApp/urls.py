83838205e6e211013d5362428640c2efd0dbce6c
lisanzatabby <lisanzatabitha@gmail.com>
2025-04-21 17:29:37 +0300

    AIU DevTandems User profile

--- AIUApp/urls.py
+++ AIUApp/urls.py
@@ -9,7 +9,8 @@ urlpatterns  = [
 
     path('', views.index, name='home'),
     path('room/<str:pk>/', views.rooms, name='room'),
-
+    path('profile/<str:pk>/', views.userProfile, name='user-profile'),
+    
     path('create-room/', views.create_room, name='create-room'),
     path('update-room/<str:pk>/', views.updateRoom, name='updateroom'),
     path('delete-room/<str:pk>/', views.deleteRoom, name='deleteroom'),
